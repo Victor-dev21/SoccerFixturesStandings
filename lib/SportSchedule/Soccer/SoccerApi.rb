0b6e77b26745612688ab6887c36579542a7d9a43
github username <github email address>
2021-11-01 15:22:23 -0400

    defined #display_all_matches_for_specific_date

--- lib/SportSchedule/Soccer/SoccerApi.rb
+++ lib/SportSchedule/Soccer/SoccerApi.rb
@@ -17,7 +17,8 @@ class SoccerApi
 		request["x-rapidapi-host"] = 'api-football-v1.p.rapidapi.com'
 		request["x-rapidapi-key"] = api_key
 		response = http.request(request)
-		response = JSON.parse(response.body.gsub("=>", ":").gsub(":nil,", ":null,"))
+
+
 	end
 	# date parameter format: "yyyy-month-day_of_month"
 	def self.fixtures(date,league_id)
@@ -26,23 +27,23 @@ class SoccerApi
 		@@fixtures = url
 	end
 	# date parameter format: "yyyy-month-day_of_month"
-	# only from top four euro leagues
+	# only from top four european leagues
 	def self.display_all_matches_for_a_specific_date(date)
 		top_leagues=[140,78,39,135]
 		top_leagues.each do |league_id|
-
 		self.fixtures(date,league_id)
 		self.create_matches_from_collection(@@fixtures)
 		end
 	end
 
-	def self.create_matches_from_collection(fixture)
+	def self.display_matches_for_a_specific_fixture(fixture)
 		response = SoccerApi.establish_connection(fixture)
-		#self.print_easy_to_read_json(response)
-		puts response['response'][1]['league']['name']
+		if(response['response'].length > 0 && response['response'][0] != nil)
+		puts response['response'][0]['league']['name']
 		response['response'].each do |team|
 			puts"#{team['teams']['home']['name']} vs #{team['teams']['away']['name']}"
 		end
+	end
 		puts
 	end
 
@@ -53,7 +54,7 @@ class SoccerApi
 end
 
 #response = SoccerApi.establish_connection(SoccerApi.fixtures("2021-10-31","140"))
-SoccerApi.display_all_matches_for_a_specific_date("2021-10-31")
+SoccerApi.display_all_matches_for_a_specific_date("2021-11-06")
 
 #puts JSON.pretty_generate(response['response'][2]['teams']['home']['name'])
 #puts JSON.pretty_generate(response['response'][2]['teams']['away']['name'])
