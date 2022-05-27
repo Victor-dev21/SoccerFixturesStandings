05d8c8db3443caf8ad4e6f0af3b3dc166e74535a
github username <github email address>
2022-05-27 18:44:46 -0400

    method added to search formula1 event by date

--- lib/Formula1/ApiCalls.rb
+++ lib/Formula1/ApiCalls.rb
@@ -3,6 +3,7 @@ require 'net/http'
 require 'openssl'
 require 'json'
 require 'date'
+require_relative "../DateParser"
 class ApiCalls
 
   @@standings = Hash.new
@@ -32,11 +33,54 @@ class ApiCalls
   def self.upcoming_race_weekends
     response = File.read(URI.open("http://ergast.com/api/f1/current.json"))
     json = JSON.parse(response)
-    json["MRData"]["RaceTable"]["races"].each do |race|
-      self.schedule[]
+    json["MRData"]["RaceTable"]["Races"].each do |race|
+      self.schedule[race["raceName"]] = Array.new
+      self.schedule[race["raceName"]] << {FirstPractice: DateParser.parse_date(race["FirstPractice"]["date"])}
+      self.schedule[race["raceName"]] << {SecondPractice: DateParser.parse_date(race["SecondPractice"]["date"])}
+      if(race["ThirdPractice"].nil?)
+        self.schedule[race["raceName"]] << {ThirdPractice: DateParser.parse_date(race["Sprint"]["date"])}
+      else
+        self.schedule[race["raceName"]] << {ThirdPractice: DateParser.parse_date(race["ThirdPractice"]["date"])}
+      end
+      self.schedule[race["raceName"]] << {Qualifying: DateParser.parse_date(race["Qualifying"]["date"])}
+      self.schedule[race["raceName"]] << {RaceDay: DateParser.parse_date(race["date"])}
     end
+    self.schedule
   end
+
+  def self.first_practice
+  end
+  def self.second_practice
+  end
+  def self.third_practice
+  end
+  def self.qualifying
+  end
+  def self.race_day
+
+  end
+  ##mm-dd-yyyy
+  def self.search_by_date(date)
+    input = DateParser.parse_input_date(date)
+    eventName = ""
+    raceName = ""
+    self.schedule.find do |key,event|
+      raceName =  key
+      eventName = event.find do |e|
+         e.values[0] = input
+      end
+
+    end
+    "#{raceName}: #{eventName.keys[0]}"
+  end
+
 end
+#"Bahrain Grand Prix"=>[{:FirstPractice=>#<Date: 2022-03-18 ((2459657j,0s,0n),+0s,2299161j)>},
+#{:SecondPractice=>#<Date: 2022-03-18 ((2459657j,0s,0n),+0s,2299161j)>},
+#{:ThirdPractice=>#<Date: 2022-03-19 ((2459658j,0s,0n),+0s,2299161j)>},
+#{:Qualifying=>#<Date: 2022-03-19 ((2459658j,0s,0n),+0s,2299161j)>},
+#{:RaceDay=>#<Date: 2022-03-20 ((2459659j,0s,0n),+0s,2299161j)>}]
 
 
 puts ApiCalls.upcoming_race_weekends
+puts ApiCalls.search_by_date("03-18-2022")
--- lib/dateParser.rb
+++ lib/dateParser.rb
@@ -1,2 +1,21 @@
+require 'date'
+require 'pry'
 class DateParser
+  def self.parse_date(input)
+		input = input.split("-")
+    month = input[1].to_i
+		date = input[2].to_i
+		year = input[0].to_i
+		Date.new(year,month,date)
+	end
+
+  def self.parse_input_date(input)
+		input = input.split("-")
+		date = input[1].to_i
+		month = input[0].to_i
+		year = input[2].to_i
+		Date.new(year,month,date)
+	end
 end
+puts DateParser.parse_date("2022-03-20")
+puts DateParser.parse_input_date("03-20-2022") == DateParser.parse_date("2022-03-20")
